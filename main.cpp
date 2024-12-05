beff72d385786a260a5c88c6d707dbbd62e18337
ThePrawnBay <moosegoose3216@gmail.com>
2024-12-05 15:28:16 -0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>//needed for getline
 using namespace std;
 //global variables: makes the variable be seen and used by ALL functions
 string inventory[10];
@@ -22,14 +23,14 @@ int main() {
 	while (input != "quit") {//game loop
 		switch (room) {
 		case 1:
-			cout << "your in room 1, you can go north" << endl;
-			cin >> input;
+			cout << "youre in room 1, you can go north" << endl;
+			getline(cin, input);
 			if (input == "north" || input == "go north")
 				room = 2;
 			break;
 		case 2:
 			cout << "your in room 2, you can go northeast, northwest, and south" << endl;
-			cin >> input;
+			getline(cin, input);
 			if (input == "northeast" || input == "go northeast")
 				room = 3;
 			if (input == "northwest" || input == "go northwest")
@@ -39,7 +40,7 @@ int main() {
 			break;
 		case 3:
 			cout << "your in room 3, you can go north, east, west, and south" << endl;
-			cin >> input;
+			getline(cin, input);
 			if (input == "north" || input == "go north")
 				room = 6;
 			if (input == "east" || input == "go east")
@@ -51,7 +52,7 @@ int main() {
 			break;
 		case 4:
 			cout << "your in room 4, you can go north, east, west, and south" << endl;
-			cin >> input;
+			getline(cin, input);
 			if (input == "north" || input == "go north")
 				room = 6;
 			if (input == "east" || input == "go east")
@@ -63,15 +64,15 @@ int main() {
 			break;
 		case 5:
 			cout << "your in room 5, you can go northeast and east" << endl;
-			cin >> input;
+			getline(cin, input);
 			if (input == "northeast" || input == "go northeast")
 				room = 6;
 			if (input == "east" || input == "go east")
 				room = 4;
 			break;
 		case 6:
-			cout << "your in room 6, you can go west, southeast, southwest, and east" << endl;
-			cin >> input;
+			cout << "your in room 6, go west, southeast, southwest, and east or you can take the dragon to the hatchery, go to the BOSS" << endl;
+			getline(cin, input);
 			if (input == "west" || input == "go west")
 				room = 5;
 			if (input == "east" || input == "go east")
@@ -80,24 +81,57 @@ int main() {
 				room = 4;
 			if (input == "southeast" || input == "go southeast")
 				room = 3;
+			if (input == "dragon" || input == "take dragon")
+				room = 8;
+			if (input == "boss" || input == "go to boss")
+				room = 11;
 			break;
 		case 7:
 			cout << "your in room 7, you can go west and southwest" << endl;
-			cin >> input;
+			getline(cin, input);
 			if (input == "west" || input == "go west")
 				room = 5;
 			if (input == "east" || input == "go east")
 				room = 7;
 			break;
+		case 8:
+			cout << "your in room 8, you can go up or down" << endl;
+			getline(cin, input);
+			if (input == "up" || input == "go up")
+				room = 9;
+			if (input == "down" || input == "go down")
+				room = 1;
+			break;
+		case 9:
+			cout << "your in room 9, you can go up and down" << endl;
+			getline(cin, input);
+			if (input == "up" || input == "go up")
+				room = 10;
+			if (input == "down" || input == "go down")
+				room = 8;
+			break;
+		case 10:
+			cout << "your in room 10, you can go down" << endl;
+			getline(cin, input);
+			if (input == "down" || input == "go down")
+				room = 9;
+			break;
+		case 11:
+			cout << "your in room 11, You have to slay the dragon" << endl;
+			if (inventory[1] == "sword") {
+				cout << "you slay the dragon with your sword and escape the castle" << endl;
+				input = "quit";
+				return 0; // kills the program
+			}
+			else {
+				cout << "you ded." << endl;
+				input = "quit";
+			}
 		}
 	}//end of game loop
 	cout << "thanks for playing" << endl;
 }//end of main
 
-
-void FirstZombieEncounter() {
-
-}
 
 void FirstZombieEncounter() {
 
